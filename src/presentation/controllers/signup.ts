53d60cb1b90e2181fe805d082bffeda514ef417f
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2023-02-20 21:15:17 -0300

    Revert "Revert "feat: ensure SignUpController return 400 if no password is provider""
    This reverts commit 378981be8ed9970084c5887aedf59a0e37081d66.

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFelds = ['name', 'email']
+    const requiredFelds = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFelds) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))

a7d0e1b83f0f1136a2fac05d3fc170eec237a2fe
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2023-02-23 22:31:00 -0300

    feat: ensure SignUpController returns 400 if password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -18,10 +18,12 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
+      }
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
       }
     } catch (error) {
       return serverError()

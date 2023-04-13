aeb6c7afd981f3a008276f2fa7d05fca457629d0
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2023-04-12 23:17:52 -0300

    test: ensure AccountMongoRepository returns null if loadByEmail returns null

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -51,4 +51,10 @@ describe('Account Mongo Repository', () => {
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
   })
+
+  test('Should return an account on add success', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('any_email@mail.com')
+    expect(account).toBeFalsy()
+  })
 })

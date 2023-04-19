feaa20b717d520966b4c69d57b9b93c3d4307fbc
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2023-04-18 23:14:55 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'fernando@gmail.com',
+          password: 'banana'
+        })
+        .expect(401)
+    })
   })
 })

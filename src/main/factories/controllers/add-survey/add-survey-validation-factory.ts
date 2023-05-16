8b04bcf6b3954fe2e30d3b0678d8674b6153fdd6
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2023-05-15 22:52:36 -0300

    chore: remove useless variables

--- src/main/factories/controllers/add-survey/add-survey-validation-factory.ts
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.ts
@@ -1,6 +1,5 @@
-import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
+import { RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
 import { Validation } from '../../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
 
 export const makeAddSurveyValidation = (): ValidationComposite => {
   const validations: Validation[] = []

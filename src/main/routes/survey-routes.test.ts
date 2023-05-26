cff76c321f4a7e7ff14ed0298cac136cb87757d1
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2023-05-26 14:14:15 -0300

    test: ensure load surveys route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -76,3 +76,80 @@ describe('Survey Routes', () => {
     })
   })
 })
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('POST/surveys', () => {
+    test('Should return 403 on add survey without accessToken', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answers 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Answers 2'
+          }]
+        })
+        .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'fernando',
+        email: 'fernando@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+
+      const { insertedId: id } = res
+      const accessToken = sign({ id }, env.jwtSecret)
+
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answers 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Answers 2'
+          }]
+        })
+        .expect(204)
+    })
+  })
+
+  describe('GET/surveys', () => {
+    test('Should return 403 on load survey with valid accessToken', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
+    })
+  })
+})

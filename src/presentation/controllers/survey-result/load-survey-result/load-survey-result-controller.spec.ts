2561892eed6c01287ed0a31055629b6dc7050884
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2023-08-01 17:29:54 -0300

    test: ensure LoadSurveyResultController calls LoadSurveyById with correct id

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -0,0 +1,33 @@
+import { LoadSurveyResultController } from './load-survey-result-controller'
+import { HttpRequest, LoadSurveyById } from './load-survey-result-controller-protocols'
+import { mockLoadSurveyById } from '@/presentation/test'
+
+describe('LoadSurveyResult Controller', () => {
+  const makeFakeRequest = (): HttpRequest => ({
+    params: {
+      surveyId: 'any_id'
+    }
+  })
+
+  type SutTypes = {
+    sut: LoadSurveyResultController
+    loadSurveyByIdStub: LoadSurveyById
+  }
+
+  const makeSut = (): SutTypes => {
+    const loadSurveyByIdStub = mockLoadSurveyById()
+    const sut = new LoadSurveyResultController(loadSurveyByIdStub)
+
+    return {
+      sut,
+      loadSurveyByIdStub
+    }
+  }
+
+  test('Should call LoadSurveyById with correct values', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    const loadSurveyById = jest.spyOn(loadSurveyByIdStub, 'loadById')
+    await sut.handle(makeFakeRequest())
+    expect(loadSurveyById).toHaveBeenCalledWith('any_id')
+  })
+})
